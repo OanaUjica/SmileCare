4b7879a738a08ebca20180fd23fe2b38b8c11a97
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2020-05-06 21:29:47 +0300

    Update unit tests.

--- PatientServiceTests/Service/PatientServiceShould.cs
+++ PatientServiceTests/Service/PatientServiceShould.cs
@@ -105,5 +105,53 @@ namespace SmileCareTests.Service
             }
 
         }
+
+
+        [Fact]
+        public void DeletePatient()
+        {
+            // Arrange - set up the test
+            var options = new DbContextOptionsBuilder<LaboratoryDbContext>()
+                .UseInMemoryDatabase("LaboratoryDatabaseForTesting")
+                .Options;
+
+            using (var context = new LaboratoryDbContext(options))
+            {
+                context.Patients.Add(new Patient()
+                {
+                    FirstName = "Remus",
+                    LastName = "Pavelean",
+                    City = "Cluj-Napoca",
+                    Email = "remus.pavelean@gmail.com",
+                    Phone = "0752436680"
+                });
+
+                context.Patients.Add(new Patient()
+                {
+                    FirstName = "Anca",
+                    LastName = "Marcus",
+                    City = "Brasov",
+                    Email = "anca.marcus@gmail.com",
+                    Phone = "0752406360"
+                });
+
+                context.SaveChanges();
+
+                var patientRepository = new PatientRepository(context);
+                var patientService = new PatientService(patientRepository);
+                patientRepository.Delete(1);
+
+
+                // Act - invoke the method we want to test
+                var patients = patientService.ReadAll();
+
+
+
+
+                //Assert - verify that the action of the tested method behaved as expected
+                Assert.Equal("Anca", patients.Find(p => p.Id == 2).FirstName);
+            }
+
+        }
     }
 }
